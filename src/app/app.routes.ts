985cf3579ab5bac42f5fc49b8c8035ff61d650fa
Francisco Jimenez <>
2026-02-16 17:37:21 -0600

    style changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Login } from './components/login/login';
+import { MonthDetail } from './components/month-detail/month-detail';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'login',
         component: Login
-    }
+    },
+    {
+        path: 'month-details',
+        component: MonthDetail
+    }    
 ];
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  constructor() {}
+
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+
+
+  onSubmit() {
+    // Aquí puedes agregar la lógica para manejar el inicio de sesión
+    console.log('Formulario de inicio de sesión enviado');
+  }
 }
